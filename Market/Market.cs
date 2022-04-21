dcf528ff080999d5924e8db4ae50bf3906ff1318
ftr <noemail>
2022-04-21 14:32:03 +0900

    Skeletonning market

--- Market/Market.cs
+++ Market/Market.cs
@@ -1,7 +1,26 @@
-﻿namespace Market
+﻿using System;
+
+namespace Market
 {
     public class Market
     {
-        
+        public void Order(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Trade()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Order
+    {
+        public enum Type
+        {
+            Buy,
+            Sell
+        }
     }
 }
