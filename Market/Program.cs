74c321518ec53f74e9453ea71b59dbaf23e26070
ftr <noemail>
2022-04-26 19:28:09 +0900

    added command line commands

--- Market/Program.cs
+++ Market/Program.cs
@@ -4,16 +4,34 @@ namespace Market
 {
     class Program
     {
+        private const string Order = "order";
+        private const string Trade = "trade";
+        private const string Quit = "quit";
+        
         static void Main(string[] args)
         {
             var csv = new CsvData<GoodPrice>(args[0]);
             csv.Read();
+            Console.WriteLine("Loaded csv");
             Console.WriteLine(csv);
-            // while (true)
-            // {
-            //     var line = Console.ReadLine();
-            //     Console.WriteLine(line);
-            // }
+            
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var command = line?.Split()[0];
+                switch (command)
+                {
+                    case Quit:
+                        return;
+                    case Order:
+                        break;
+                    case Trade:
+                        break;
+                    default:
+                        Console.WriteLine($"{command} is not valid");
+                        break;
+                }
+            }
         }
 
         private class GoodPrice
