2bde2c61b4a19425383fb4f0ca05703a59108cc8
ftr <noemail>
2022-04-28 17:48:30 +0900

    Restructured tests

---
+++ Tests/GoodMarket/ForBookWithOrders.cs
@@ -0,0 +1,78 @@
+﻿using Market;
+using NUnit.Framework;
+
+namespace Tests.GoodMarket
+{
+    public class ForBookWithOrders
+    {
+        protected TestMarketFac marketFac = new TestMarketFac("oil");
+        protected Market.GoodMarket market;
+
+        [SetUp]
+        public void Setup()
+        {
+            marketFac.Reset();
+
+            marketFac.AddOrder(Order.OrderType.Bid, 10, 10);
+            marketFac.AddOrder(Order.OrderType.Bid, 9, 10);
+            marketFac.AddOrder(Order.OrderType.Bid, 8, 10);
+
+            marketFac.AddOrder(Order.OrderType.Offer, 11, 10);
+            marketFac.AddOrder(Order.OrderType.Offer, 12, 10);
+            marketFac.AddOrder(Order.OrderType.Offer, 13, 10);
+
+            market = marketFac.Market;
+        }
+
+        public class DefaultTests : ForBookWithOrders
+        {
+            [Test]
+            public void Construct()
+            {
+                Assert.Pass($"{market}");
+            }
+
+            [Test]
+            public void AddBidToBookWithNoMatch()
+            {
+                var bid = marketFac.AddOrder(Order.OrderType.Bid, 10, 5);
+                Assert.True(market.Bids[1].Equals(bid));
+            }
+
+            [Test]
+            public void AddOfferToBookWithNoMatch()
+            {
+                var offer = marketFac.AddOrder(Order.OrderType.Offer, 11, 5);
+                Assert.True(market.Offers[1].Equals(offer));
+            }
+        }
+
+        public class WhenImmediateBidResolve: ForBookWithOrders
+        {
+            private Order bid;
+            
+            [SetUp]
+            public new void Setup()
+            {
+                base.Setup();
+                bid = marketFac.AddOrder(Order.OrderType.Bid, 11, 5);
+            }
+
+            [Test]
+            public void MatchBidWithOneOffer()
+            {
+                Assert.AreEqual(5, market.Offers[0].AmountLeft);
+            }
+            
+            [Test]
+            public void MatchBidAndResolve()
+            {
+                Assert.AreNotEqual(bid, market.Bids[0]);
+            }
+        }
+
+        //offer also resolved
+        //2or more offer
+        //vice versa
+    }
+}
