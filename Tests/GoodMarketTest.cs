093618dde7121629ba9648815f04f90e794be3ea
ftr <noemail>
2022-04-28 11:33:05 +0900

    GoodMarketTest.AddHighestBid

--- Tests/GoodMarketTest.cs
+++ Tests/GoodMarketTest.cs
@@ -10,12 +10,14 @@ namespace Tests
         private Good good = new Good("Oil");
         private Good wrongGood = new Good("Cotton");
         private Order bid0;
+        private Order bid1;
         private Order wrongBid;
 
         [SetUp]
         public void Setup()
         {
             bid0 = new Order(good, 10, 10, Order.OrderType.Bid, null);
+            bid1 = new Order(good, 11, 10, Order.OrderType.Bid, null);
             wrongBid = new Order(wrongGood, 10, 10, Order.OrderType.Bid, null);
             testMarket = new GoodMarket(good);
         }
@@ -40,5 +42,16 @@ namespace Tests
                 testMarket.TakeOrder(wrongBid);
             });
         }
+
+        [Test]
+        public void AddHighestBid()
+        {
+            testMarket.TakeOrder(bid0);
+            testMarket.TakeOrder(bid1);
+            Assert.True(testMarket.Bids[0].Equals(bid1));
+        }
+        
+        //lowest
+        //middle
     }
 }
